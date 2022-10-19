8f0016920c12a85aabad672fcb49a8ff28336f18
AdityaParashar24 <adityaparashar488@gmail.com>
2022-10-19 17:43:07 +0100

    echo added in dictionary

--- src/applications/application.py
+++ src/applications/application.py
@@ -4,7 +4,7 @@ from abc import abstractmethod, ABC
 class Application(ABC):
     @abstractmethod
     def exec(self):
-        self.exec()
+        pass
 
 
 class Pwd(Application):
@@ -13,3 +13,10 @@ class Pwd(Application):
 
     def exec(self):
         print("hi i am pwd")
+
+class Echo(Application):
+    def __init__(self):
+        pass
+
+    def exec(self):
+        print("hi i am echo")
--- src/applications/application_factory.py
+++ src/applications/application_factory.py
@@ -1,10 +1,11 @@
-from applications.application import Application, Pwd
+import applications.application as application
 
 
 class ApplicationFactory:
     def __init__(self):
-        self.app_types = {'pwd': Pwd
+        self.app_types = {'pwd': application.Pwd,
+                          'echo': application.Echo
                           }
 
-    def app_by_name(self, name: str) -> Application:
+    def app_by_name(self, name: str) -> application.Application:
         return self.app_types[name]()
