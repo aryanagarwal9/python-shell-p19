baf1ce2fc7977d99ebe464490e14c172562e64a4
AdityaParashar24 <adityaparashar488@gmail.com>
2022-11-02 14:51:47 +0000

    completed sort applicaton, improved grep

--- src/applications/app_grep.py
+++ src/applications/app_grep.py
@@ -4,31 +4,28 @@ from collections import deque
 
 from applications.application import Application
 
+
 class Grep(Application):
     def exec(self, args: list, stdin: Optional[list], out: deque):
         if not len(args):
             raise ValueError("No arguments provided")
 
+        self.call_required_function(args, stdin, out)
+
+    def call_required_function(self, args: list, stdin: Optional[list], out: deque):
         pattern = args[0]
-
-        self.call_required_function(pattern, stdin, args, out)
-
-    def call_required_function(self, pattern, stdin, args, out):
         num_args = len(args)
-        match num_args:
-            case 1:
-                raise ValueError("No files provided, std input can't be taken from keyboard")
-            case 2:
-                self.handle_grep_stdin(pattern, stdin, out)
-            case other:
-                self.handle_grep_file_input(pattern, args, out)
+        if num_args > 1:
+            self.handle_file_input(pattern, args, out)
+        else:
+            self.handle_stdin(pattern, stdin, out)
 
-    def handle_grep_stdin(self, pattern: str, stdin: Optional[list], out: deque):
+    def handle_stdin(self, pattern: str, stdin: Optional[list], out: deque):
         for input_string in stdin:
             if re.match(pattern, input_string):
-                out.append(input_string+'\n')
+                out.append(input_string + '\n')
 
-    def handle_grep_file_input(self, pattern: str, args: list, out:deque):
+    def handle_file_input(self, pattern: str, args: list, out: deque):
         files = args[1:]
         num_files = len(files)
         for file_name in files:
@@ -36,8 +33,4 @@ class Grep(Application):
                 file_lines = file.readlines()
                 for line in file_lines:
                     if re.match(pattern, line):
-                        out.append(f"{file_name}:{line}\n") if num_files>1 else out.append(line+'\n')
-
-
-
-
+                        out.append(f"{file_name}:{line}\n") if num_files > 1 else out.append(line + '\n')
