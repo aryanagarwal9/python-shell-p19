8dce086e37beeb007499bec7a36190443d7335ff
aryanagarwal9 <59826369+aryanagarwal9@users.noreply.github.com>
2022-11-16 14:45:25 +0000

    Added utils.py

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import errors
+
+def check_flag(arg: str, flag: str):
+    if arg != flag:
+        raise errors.FlagError("Invalid flags given")
+
+    return True
+
+
+def check_stdin(stdin: Optional[list]):
+    if stdin is None:
+        raise errors.StandardInputError("No input given")
+
+    return True
+
+
+def split_stdin_to_lines(stdin):
+    lines = []
+    start = 0
+    while True:
+        if start >= len(stdin) - 1:
+            break
+
+        i = stdin.find('\n', start)
+        if i == -1:
+            lines.append(stdin[start:])
+            break
+        else:
+            end = i + 1
+            lines.append(stdin[start: end])
+            start = end
+
+    return lines
