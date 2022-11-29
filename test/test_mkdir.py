23e4731f464d475a0c6cfa2b06c06a3641db6467
AdityaParashar24 <adityaparashar488@gmail.com>
2022-11-29 22:36:28 +0000

    mkdir tests

--- test/test_mkdir.py
+++ test/test_mkdir.py
@@ -2,10 +2,12 @@ import os
 import shutil
 import unittest
 from collections import deque
+
 from parameterized import parameterized
 
 from src.applications.app_mkdir import Mkdir
-from src.applications.unsafe_decorator import UnsafeDecorator
+
+
 class TestMkdir(unittest.TestCase):
     def setUp(self) -> None:
         self.out = deque()
@@ -28,22 +30,28 @@ class TestMkdir(unittest.TestCase):
         self.assertEqual(expected_output, sorted(args))
 
     @parameterized.expand([
-        ['single directory', ['dir1/dir2']],
-        ['multiple directories', ['dir1/dir2', 'dir1/dir2/dir3']]
+        ['single directory', ['dir1/dir2'], ['dir1/dir2']],
+        ['multiple directories', ['dir1/dir2', 'dir1/dir2/dir3'],
+         ['dir1/dir2', 'dir1/dir2/dir3']]
     ])
-    def test_mkdir_sub_directories(self, name, args):
+    def test_mkdir_sub_directories(self, name, args, result):
         os.mkdir('dir1')
         Mkdir().exec(args, None, self.out)
         expected_output = sorted([elem[0] for elem in os.walk('dir1')])
-        self.assertEqual(expected_output, sorted(['dir1']+args))
+        expected_output.remove('dir1')
+        self.assertEqual(expected_output, sorted(result))
 
-    def test_mkdir_flag_final_directory(self):
-        Mkdir().exec(['-p','dir1/dir2'], None, self.out)
-        expected_output = os.listdir('dir1')
-        self.assertEqual(expected_output, ['dir2'])
+    @parameterized.expand([
+        ['single directory', ['-p', 'dir1/dir2'], ['./dir1', './dir1/dir2']],
+        ['multiple directories',
+         ['-p', 'dir1/dir2', 'dir2/dir3/dir4', 'dir1/dir5/dir7/dir2'],
+         ['./dir1', './dir1/dir2', './dir1/dir5', './dir1/dir5/dir7',
+          './dir1/dir5/dir7/dir2', './dir2', './dir2/dir3',
+          './dir2/dir3/dir4']]
+    ])
+    def test_mkdir_parent_flag(self, name, args, result):
+        Mkdir().exec(args, None, self.out)
+        expected_output = sorted([elem[0] for elem in os.walk('.')])
+        expected_output.remove('.')
+        self.assertEqual(expected_output, sorted(result))
 
-    def test_mkdir_unsafe_create_new_directories_even_if_some_arguments_exist(self):
-        os.mkdir('dir1')
-        self.assertEqual(UnsafeDecorator(Mkdir()).exec(['dir1','dir2','dir3'], None, self.out), 'FileExistsError\n')
-        expected_output = sorted(os.listdir('dir1'))
-        self.assertEqual(expected_output, ['dir2','dir3'])
