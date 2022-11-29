a3973fbcbf87437382ac60136f6a4893a57e036b
aryanagarwal9 <59826369+aryanagarwal9@users.noreply.github.com>
2022-11-29 22:36:45 +0000

    Update test_grep.py

--- test/test_grep.py
+++ test/test_grep.py
@@ -46,12 +46,37 @@ class TestGrep(unittest.TestCase):
         self.assertEqual(list(self.out), result)
 
     @parameterized.expand([
+        ['one_file_input', ['-v', 'app', 'resources/test1.txt'],
+         ['grep is used to search for a pattern\n', 'Used in shell\n']],
+        ['multiple_file_input',
+         ['-v', 'is', 'resources/test1.txt', 'resources/test2.txt',
+          'resources/test3.txt'], [
+             'resources/test1.txt:Used in shell\n',
+             'resources/test2.txt:I enjoyed the experience\n',
+             'resources/test3.txt:class for testing grep\n',
+             'resources/test3.txt:making unit tests to test app\n']]
+    ])
+    def test_grep_invert_match_file_input(self, name, args, result):
+        Grep().exec(args, None, self.out)
+        self.assertEqual(list(self.out), result)
+
+    @parameterized.expand([
         ['no_match_present', ['hel'], 'hi\nI hope you have a good day', []],
         ['matches_present', ['hel'],
          'hello\nI am glad to see you\nLets work on shell\n',
          ['hello\n', 'Lets work on shell\n']]
     ])
     def test_grep_stdin(self, name, args, stdin, result):
+        Grep().exec(args, stdin, self.out)
+        self.assertEqual(list(self.out), result)
+
+    @parameterized.expand([
+        ['one_file_input', ['-v', 'grep'], 'Use grep\nUsed in shell\n',
+         ['Used in shell\n']],
+        ['multiple_file_input', ['-v', 'is'], 'Grep is good\nUsed in shell\n',
+         ['Used in shell\n']]
+    ])
+    def test_grep_invert_match_stdin(self, name, args, stdin, result):
         Grep().exec(args, stdin, self.out)
         self.assertEqual(list(self.out), result)
 
