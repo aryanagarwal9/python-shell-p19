31a05f1b3e182f9bb9c122affae8957e7e128f2d
AdityaParashar24 <adityaparashar488@gmail.com>
2022-11-30 13:27:11 +0000

    all_done

--- src/shell_commands/parser_error_listener.py
+++ src/shell_commands/parser_error_listener.py
@@ -1,11 +1,12 @@
 from antlr4.error.ErrorListener import ErrorListener
-
+from antlr4.error.Errors import ParseCancellationException
 
 class ParserErrorListener(ErrorListener):
     INSTANCE = None
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        print("line " + str(line) + ":" + str(column) + " " + msg)
+        error_msg = f' Does not follow the grammar: line: {line}, column: {column}, msg: {msg}'
+        raise ParseCancellationException(error_msg)
 
 
 ParserErrorListener.INSTANCE = ParserErrorListener()
